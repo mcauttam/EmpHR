010d85ac3221ed52cbc5a3f0bc17a972f6b3222b
mcauttam <mca.uttam@gmail.com>
2021-07-19 13:06:05 +0530

    Created First V1

---
+++ EmpHR/EmpHR/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EmpHR.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
